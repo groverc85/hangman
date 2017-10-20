97d2cb38b037979b7665f3d29bd42a7172a0ebeb
Grover Chen <groverchen1994@gmail.com>
2017-10-20 15:26:38 -0400

    update game.spec.js

--- test/game.spec.js
+++ test/game.spec.js
@@ -1,46 +1,52 @@
-describe('guessLetterService factory', function() {
-	var guessLetterService;
+/*global describe, it */
+(function(){
+    "use strict";
 
-	beforeEach(angular.mock.module('app'));
-	// Before each test set our injected guessLetterService factory (_guessLetterService_) to our local variable
-	beforeEach(inject(function(_guessLetterService_) {
-		guessLetterService = _guessLetterService_;
-	}));
+	describe('guessLetterService factory', function() {
+		var guessLetterService;
 
-	// A simple test to verify the guessLetterService factory exists
-	it('should exist', function() {
-		expect(guessLetterService).toBeDefined();
+		beforeEach(angular.mock.module('app'));
+		// Before each test set our injected guessLetterService factory (_guessLetterService_) to our local variable
+		beforeEach(inject(function(_guessLetterService_) {
+			guessLetterService = _guessLetterService_;
+		}));
+
+		// A simple test to verify the guessLetterService factory exists
+		it('should exist', function() {
+			expect(guessLetterService).toBeDefined();
+		});
 	});
-});
 
 
-describe('randomWordService factory', function() {
-	var randomWordService;
+	describe('randomWordService factory', function() {
+		var randomWordService;
 
-	beforeEach(angular.mock.module('app'));
+		beforeEach(angular.mock.module('app'));
 
-  	// Before each test set our injected guessLetterService factory (_randomWordService_) to our local variable
-  	beforeEach(inject(function(_randomWordService_) {
-    	randomWordService = _randomWordService_;
-  	}));
+	  	// Before each test set our injected guessLetterService factory (_randomWordService_) to our local variable
+	  	beforeEach(inject(function(_randomWordService_) {
+	    	randomWordService = _randomWordService_;
+	  	}));
 
-  	// A simple test to verify the randomWordService factory exists
-  	it('should exist', function() {
-	    expect(randomWordService).toBeDefined();
-  	});
-});
-
-
-describe('hangmanController', function () {
-	var $controller;
-
-	beforeEach(angular.mock.module('app'));
-
-	beforeEach(angular.mock.inject(function(_$controller_){
-	  $controller = _$controller_;
-	}));
-
-	it('should exist', function() {
-		expect($controller).toBeDefined();
+	  	// A simple test to verify the randomWordService factory exists
+	  	it('should exist', function() {
+		    expect(randomWordService).toBeDefined();
+	  	});
 	});
-});
+
+
+	describe('hangmanController', function () {
+		var $controller;
+
+		beforeEach(angular.mock.module('app'));
+
+		beforeEach(angular.mock.inject(function(_$controller_){
+		  $controller = _$controller_;
+		}));
+
+		it('should exist', function() {
+			expect($controller).toBeDefined();
+		});
+	});
+
+})();
